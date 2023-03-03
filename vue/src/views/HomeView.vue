3e820fc53f9a7c62780e77e6fbc68375446aa781
mr. whalen <michael.whalen@sitechhs.com>
2023-03-03 08:41:58 -0500

    dfgh

---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ student }}</h1>
+  <ul>
+    <li v-for="animal in animals" :key="animal">{{ animal }}</li>
+  </ul>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      student: "Daria",
+      animals: ["pig", "cow", "zebra", "Bogdan"],
+    };
+  },
+};
+</script>
