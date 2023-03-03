a8adedcc0a92e7c36e043a0861f572c10a69cfe4
mr. whalen <michael.whalen@sitechhs.com>
2023-03-03 13:21:57 -0500

    sfdghdfgh

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,10 +1,18 @@
 <template>
-  <h1 v-if="graduated">Congrats!</h1>
-  <h1 v-else>You'll get em next time</h1>
+  <h1 v-if="selected === 'A'">Wrong</h1>
+  <h1 v-else-if="selected === 'B'">Wronger</h1>
+  <h1 v-else-if="selected === 'C'">Right</h1>
+  <h1 v-else="selected === ''">Answer A,B,C</h1>
   <h1>{{ student }}</h1>
   <ul>
     <li v-for="animal in animals" :key="animal">{{ animal }}</li>
   </ul>
+  <select v-model="selected">
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+  </select>
+  <h2>{{ selected }}</h2>
 </template>
 <script>
 export default {
@@ -13,6 +21,8 @@ export default {
       graduated: false,
       student: "Yifan",
       animals: ["pig", "cow", "ostrich", "Leon"],
+      text: "",
+      selected: "",
     };
   },
 };
