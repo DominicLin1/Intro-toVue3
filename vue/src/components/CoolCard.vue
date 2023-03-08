c6ec1f0cdad8e8af8e482d7e898e26e875fe299a
mr. whalen <michael.whalen@sitechhs.com>
2023-03-08 07:09:26 -0500

    dfg

---
+++ vue/src/components/CoolCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>{{ animal.title }}</h2>
+    <p>{{ animal.desc }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CoolCard",
+  props: {
+    animal: Object,
+  },
+};
+</script>
+
+<style scoped>
+div {
+  height: 500px;
+  width: 300px;
+  background-color: red;
+  color: white;
+}
+</style>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <template>
-  <h1>{{ student }}</h1>
+  <CoolButton @click="test">Click Me</CoolButton>
+  <CoolCard v-for="animal in animals" :key="animal.title" :animal="animal" />
+  <!--  <h1>{{ student }}</h1>
   <ul>
     <li v-for="animal in animals" :key="animal">{{ animal }}</li>
   </ul>
-  <!-- <input v-model="text" />
-  <p>{{ text }}</p> -->
+
   <select v-model="selected">
     <option>A</option>
     <option>B</option>
@@ -13,18 +14,32 @@
   <h1 v-if="selected === 'A'">You are correct</h1>
   <h1 v-else-if="selected === ''">Please Answer</h1>
   <h1 v-else>You are Wrong</h1>
-  <h2 v-if="graduated">You have graduated</h2>
+  <h2 v-if="graduated">You have graduated</h2> -->
 </template>
 <script>
+import CoolCard from "../components/CoolCard.vue";
+import CoolButton from "../components/CoolButton.vue";
 export default {
+  components: {
+    CoolCard,
+    CoolButton,
+  },
   data() {
     return {
       student: "Daria",
-      animals: ["pig", "cow", "zebra", "Bogdan"],
+      animals: [
+        { title: "cat", desc: "sneaky animal who poops in my house" },
+        { title: "Boggy-Woggy", desc: "Stands too close to his teachers" },
+      ],
       selected: "",
       graduated: false,
       // text: "",
     };
+  },
+  methods: {
+    test: function () {
+      console.log("This is a test");
+    },
   },
 };
 </script>
